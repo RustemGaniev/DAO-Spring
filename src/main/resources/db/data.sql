116f8ed3f174114dc786b278e19435bfe0e996ac
RustemGaniev <ganiev@cora.local>
2022-07-22 12:23:49 +0300

    22 July

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO CUSTOMERS (name, surname, age, phone_number)
+VALUES ('rustem', 'ganiev', 25, 999999);
+INSERT INTO CUSTOMERS (name, surname, age, phone_number)
+VALUES ('rinat', 'ivanov', , 181818);
+INSERT INTO CUSTOMERS (name, surname, age, phone_number)
+VALUES ('vlad', 'ivleev', 45, 323232);
+
+INSERT INTO ORDERS (date, customer_id, product_name, amount)
+VALUES (now(), 2, 'iphonex', 1700);
+INSERT INTO ORDERS (date, customer_id, product_name, amount)
+VALUES (now(), 1, 'samsung71', 4500);
+INSERT INTO ORDERS (date, customer_id, product_name, amount)
+VALUES (now(), 3, 'Xiaomiredmi', 7400);
